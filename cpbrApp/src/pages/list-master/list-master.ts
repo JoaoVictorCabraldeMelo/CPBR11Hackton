2f3b10351f0553ff17e47be5a7e3aa08a4c67399
igorLisboa <igorlisboa34@gmail.com>
2018-02-03 18:48:29 -0200

    tentativa de usar api, com alguns problemas, sera concertado ainda

--- cpbrApp/src/pages/list-master/list-master.ts
+++ cpbrApp/src/pages/list-master/list-master.ts
@@ -3,6 +3,7 @@ import { IonicPage, ModalController, NavController } from 'ionic-angular';
 
 import { Item } from '../../models/item';
 import { Items } from '../../providers/providers';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -12,43 +13,68 @@ import { Items } from '../../providers/providers';
 export class ListMasterPage {
   currentItems: Item[];
 
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
+  url:string= 'https://sandboxapi.campuse.ro/event/list/';
+  lista:any;
+
+  constructor(private http: Http,public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
     this.currentItems = this.items.query();
+
+    getItens();
+
   }
 
-  /**
-   * The view loaded, let's query our items for the list
-   */
-  ionViewDidLoad() {
+getItens (): Observable<Client[]> {
+    return this.http.get(this.url)
+                    .map(this.extractData)
+                    .catch(this.handleError);
   }
 
-  /**
-   * Prompt the user to add a new item. This shows our ItemCreatePage in a
-   * modal and then adds the new item to our data source if the user created one.
-   */
-  addItem() {
-    let addModal = this.modalCtrl.create('ItemCreatePage');
-    addModal.onDidDismiss(item => {
-      if (item) {
-        this.items.add(item);
-      }
-    })
-    addModal.present();
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.results || { };
   }
 
-  /**
-   * Delete an item from the list of items.
-   */
-  deleteItem(item) {
-    this.items.delete(item);
+  private handleError (error: any) {
+    let errMsg = 'mensagem de erro';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
+
+
+  // /**
+  //  * The view loaded, let's query our items for the list
+  //  */
+  // ionViewDidLoad() {
+  // }
+  //
+  // /**
+  //  * Prompt the user to add a new item. This shows our ItemCreatePage in a
+  //  * modal and then adds the new item to our data source if the user created one.
+  //  */
+  // addItem() {
+  //   let addModal = this.modalCtrl.create('ItemCreatePage');
+  //   addModal.onDidDismiss(item => {
+  //     if (item) {
+  //       this.items.add(item);
+  //     }
+  //   })
+  //   addModal.present();
+  // }
+
   /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
-  }
+  //  * Delete an item from the list of items.
+  //  */
+  // deleteItem(item) {
+  //   this.items.delete(item);
+  // }
+  //
+  // /**
+  //  * Navigate to the detail page for this item.
+  //  */
+  // openItem(item: Item) {
+  //   this.navCtrl.push('ItemDetailPage', {
+  //     item: item
+  //   });
+  // }
 }
